af7cb1d0978193a7097a124b0a279d88f99a1f8e
Kyriakos Bakalis <greekgodsocrates.com>
2024-01-30 10:30:29 -0500

    error page updated

--- src/Components/ImageDisplay/ImageDisplay.js
+++ src/Components/ImageDisplay/ImageDisplay.js
@@ -10,26 +10,22 @@ function ImageDisplay({ isImageGenerated, isLoading, generatedImage, imagePrompt
         return <h3>{isLoading ? "Generating..." : "Enter your prompt and click generate"}</h3>;
     }
 
-    // if (Array.isArray(generatedImage) && generatedImage.length > 0 && generatedImage[0].b64_json) {
-    //     const base64Image = generatedImage[0].b64_json;
-
-    //     console.log("Base64 Image:", base64Image);
-
-        if (typeof generatedImage === "string" && generatedImage.startsWith("data:image")) {
-            console.log("Generated Image:", generatedImage);
-            return (
-                <div className="generated-image-container">
-                    <img src={generatedImage} alt={imagePrompt || "Generated Image"} />
-                </div>
-            );
+    if (typeof generatedImage === "string" && generatedImage.startsWith("data:image")) {
+        console.log("Generated Image:", generatedImage);
+        return (
+            <div className="generated-image-container">
+                <img src={generatedImage} alt={imagePrompt || "Generated Image"} />
+            </div>
+        );
 
 
-        }
     }
+    setTimeout(() => navigate("/error"), 0);
 
-    // setTimeout(() => navigate("/error"), 0);
+    return null;
+}
 
-    // return null;
+
 
 
 export default ImageDisplay;
--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -5,6 +5,7 @@ function ErrorPage() {
   return (
     <div className="errorpage">
        <h2 className="errorpage-text">Image not available or format not recognized. Return to home page and try again.</h2>
+       <button className="errorpage-button" onClick={() => window.location.href = "/"}>Return to Home</button>
     </div>
   );
 }
