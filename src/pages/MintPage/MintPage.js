a58364920f55eb4a59abc324e2e85dc00cdeee99
Kyriakos Bakalis <greekgodsocrates.com>
2024-01-31 09:59:57 -0500

    refactoring, minting feature not functional yet

--- src/pages/MintPage/MintPage.js
+++ src/pages/MintPage/MintPage.js
@@ -1,21 +1,63 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import { useState } from 'react';
+import { ThirdwebSDK } from '@thirdweb-dev/sdk';
+import { useSDK, useAddress } from '@thirdweb-dev/react';
 import './MintPage.scss';
 
 function MintPage() {
     const navigate = useNavigate();
     const location = useLocation();
+    const sdk = new ThirdwebSDK("sepolia");
+    const address = useAddress();
     const { generatedImage, imagePrompt } = location.state || {};
+    const [isMinting, setIsMinting] = useState(false);
+    const [isMinted, setIsMinted] = useState(false);
 
-    const handleRegenerate = () => {
+
+    const handleRegenerate = (imageData) => {
         navigate('/');
     }
 
-    const handleMint = () => {
+    const handleMint = async () => {
+        setIsMinting(true);
+        try {
 
-        console.log('Minting the image...');
+            const base64Response = await fetch(generatedImage);
+            const blob = await base64Response.blob();
+            const file = new File([blob], "image.png", { type: "image/png" });
+            const imageURI = await sdk.storage.upload(file);
 
+            if (!imageURI) {
+                throw new Error('Failed to upload image');
+            }
+
+            const response = await fetch("http://localhost:8080/api/mintAPI/mint", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    userImage: imageURI,
+                    address: address,
+                    prompt: imagePrompt,
+                }),
+            });
+
+
+            if (!response.ok) {
+                throw new Error('Failed to mint image');
+            }
+            else {
+                setIsMinting(false);
+                setIsMinted(true);
+                alert ("NFT minted successfully!");
+            }
+        }
+        catch (error) {
+            console.error('Error:', error);
+        }
     };
 
     useEffect(() => {
@@ -31,7 +73,9 @@ function MintPage() {
             </div>
             <div className="image-actions">
                 <button className="regenerate-button" onClick={handleRegenerate}>Re-generate</button>
-                <button className="mint-button" onClick={handleMint}>Mint</button>
+                <button className="mint-button" onClick={handleMint}>
+                    {isMinting ? "Minting..." : "Mint"}
+                </button>
             </div>
         </div>
     );
